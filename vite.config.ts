6a78da1e4be6dddf803db5701bb823ba53e42fec
zahar1143123 <zaharubanik2007@gmail.com>
2026-07-08 10:46:41 +0300

    chore: add base path for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-
 function figmaAssetResolver() {
   return {
     name: 'figma-asset-resolver',
@@ -17,10 +16,13 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
+  // Добавляем базовый путь для GitHub Pages
+  base: '/Curriculum_Vitae/', 
+
   plugins: [
     figmaAssetResolver(),
     // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used вЂ“ do not remove them
+    // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
   ],
